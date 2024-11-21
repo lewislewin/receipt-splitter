08ffec590d5b937850f1c3cdb749a0d64bedbf79
Lewis Lewin <lewis@lewislewin.com>
2024-11-21 21:40:08 +0000

    cf changes

--- src/routes/api/parse/+server.ts
+++ src/routes/api/parse/+server.ts
@@ -151,8 +151,9 @@ export const OPTIONS = () => {
     status: 204,
     headers: {
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Max-Age': '86400',
     },
   });
 };
