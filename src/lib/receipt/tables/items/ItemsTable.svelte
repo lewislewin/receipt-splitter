fa8175e7cf23bb0607e08a7e8b570c8e97f3a7ff
Lewis Lewin <lewis@lewislewin.com>
2024-11-24 15:38:23 +0000

    reordered for mobile

--- src/lib/receipt/tables/items/ItemsTable.svelte
+++ src/lib/receipt/tables/items/ItemsTable.svelte
@@ -14,22 +14,19 @@
   <table class="table-auto w-full text-left border-collapse">
     <thead>
       <tr class="bg-gray-200 text-gray-600 uppercase text-sm">
-        <th class="px-4 py-2 text-right">Line Price</th>
-        <th class="px-4 py-2">Item Name</th>
-        <th class="px-4 py-2 text-right">Individual Price</th>
-        <th class="px-4 py-2 text-center">Actions</th>
+        <th class="px-1 py-2">Item Name</th>
+        <th class="px-1 py-2 text-right">Line Price</th>
+        <th class="px-1 py-2 text-center">Actions</th>
+        <th class="px-1 py-2 text-right">Individual Price</th>
       </tr>
     </thead>
     <tbody>
       {#each items as item, index}
         <tr class="border-t">
-          <!-- Line Price -->
-          <td class="px-4 py-2 text-right font-semibold">
-            £{calculateRowTotal(item)}
-          </td>
+          
 
           <!-- Item Name -->
-          <td class="px-4 py-2">
+          <td class="px-1 py-2">
             {#if canEdit}
               <input
                 type="text"
@@ -42,8 +39,23 @@
             {/if}
           </td>
 
+          <!-- Line Price -->
+          <td class="px-1 py-2 text-right font-semibold">
+            £{calculateRowTotal(item)}
+          </td>
+          
+
+          <!-- Actions -->
+          <td class="px-1 py-2 text-center">
+            <ItemActions
+              {item}
+              {canEdit}
+              update={(updatedValue: ReceiptItem) => onItemUpdate(index, updatedValue)}
+              remove={() => onItemRemove(index)}
+            />
+          </td>
           <!-- Individual Price -->
-          <td class="px-4 py-2 text-right">
+          <td class="px-1 py-2 text-right">
             {#if canEdit}
               <input
                 type="number"
@@ -57,16 +69,6 @@
             {:else}
               <span class="text-gray-800">£{(item.price ?? 0).toFixed(2)}</span>
             {/if}
-          </td>
-
-          <!-- Actions -->
-          <td class="px-4 py-2 text-center">
-            <ItemActions
-              {item}
-              {canEdit}
-              update={(updatedValue: ReceiptItem) => onItemUpdate(index, updatedValue)}
-              remove={() => onItemRemove(index)}
-            />
           </td>
         </tr>
       {/each}
